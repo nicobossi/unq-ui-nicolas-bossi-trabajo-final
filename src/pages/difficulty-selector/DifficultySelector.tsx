3a1b496eaa2dfcc321de1fbf54cb472f4a7b7bc4
Nicolás Bossi <nbossi@hotmail.com>
2025-12-02 20:38:19 -0300

    Se agregó el logo en la selección de dificultad y se modificó el degradé de fondo de los contenedores

--- src/pages/difficulty-selector/DifficultySelector.tsx
+++ src/pages/difficulty-selector/DifficultySelector.tsx
@@ -5,6 +5,7 @@ import type { Difficulties } from "../../types/Difficulties";
 import './difficulty-selector.css';
 import { QUESTION_GAME } from "../../routes";
 import { useNavigate } from "react-router-dom";
+import image from '../../assets/R.png'
 
 
 const DifficultySelector = () => {
@@ -25,15 +26,17 @@ const DifficultySelector = () => {
     return (
         <section className = "difficulty-selector">
             <div className = "difficulty-selector_container">
-                <div className = "difficulty-selector_title-container">
-                <h1>Preguntados</h1>
-            </div>
-            <div className = "difficulty-selector_options">
-                <DifficultyButton name = {difficulties?.option1} event = {handleDifficultySelector} />
-                <DifficultyButton name = {difficulties?.option2} event = {handleDifficultySelector} />
-                <DifficultyButton name = {difficulties?.option3} event = {handleDifficultySelector} />
-                <DifficultyButton name = {difficulties?.option4} event = {handleDifficultySelector} />
-            </div>
+                <div className = "difficulty-selector_logo-container">
+                    <figure className = "main-logo_container">
+                        <img src = {image} alt="logo principal de preguntados" />
+                    </figure>
+                </div>
+                <div className = "difficulty-selector_options">
+                    <DifficultyButton name = {difficulties?.option1} event = {handleDifficultySelector} />
+                    <DifficultyButton name = {difficulties?.option2} event = {handleDifficultySelector} />
+                    <DifficultyButton name = {difficulties?.option3} event = {handleDifficultySelector} />
+                    <DifficultyButton name = {difficulties?.option4} event = {handleDifficultySelector} />
+                </div>
             </div>
         </section>
     )
