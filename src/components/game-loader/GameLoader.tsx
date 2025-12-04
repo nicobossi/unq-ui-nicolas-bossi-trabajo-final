fca27adf1fe00f125c2d4d86ac6d27d769eab82e
Nicolás Bossi <nbossi@hotmail.com>
2025-12-04 20:55:09 -0300

    Se disminuyó la opacidad y se hizo el fondo más opcado del componente de carga

--- src/components/game-loader/GameLoader.tsx
+++ src/components/game-loader/GameLoader.tsx
@@ -3,8 +3,11 @@ import './game-loader.css'
 
 const GameLoader = () => {
     return (
-        <section className = "loader_container">
-            <p>Cargando...</p>
+        <section className = "loader">
+            <div className = "loader-container">
+                <div className = "loader-container_content"></div>
+                <p>Cargando...</p>
+            </div>
         </section>
     )
 }
